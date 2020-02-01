9391822e1c775d13eb6ac0fd02c11638ce877f57
MT-Nichols <33322116+MT-Nichols@users.noreply.github.com>
2020-02-01 20:25:57 +0800

    Check on title movment

---
+++ GGJ2020_Unity/GGJ2020_Kaya/Assets/TitleSlide_Script.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleSlide_Script : MonoBehaviour
+{
+    public Vector2 finalPos;
+    public float moveSpeed;
+
+    private RectTransform transform;
+    private bool moving;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = GetComponent<RectTransform>();
+        moving = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y > finalPos.y)
+        {
+            Vector3 newPos = transform.position;
+            newPos += Vector3.down * moveSpeed;
+            transform.position = newPos;
+            //transform.Translate((Vector3.down * Time.deltaTime) * moveSpeed);
+        }
+        else
+        {
+            moving = false;
+        }
+        
+    }
+}
