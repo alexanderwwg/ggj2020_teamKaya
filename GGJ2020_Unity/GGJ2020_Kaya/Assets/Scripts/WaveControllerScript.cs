e38c20c00c5b7a238e3ed644638adae7e52cb96c
Olivia <OliviaVespera@gmail.com>
2020-02-02 04:13:56 +0800

    revert working gameloop

--- GGJ2020_Unity/GGJ2020_Kaya/Assets/Scripts/WaveControllerScript.cs
+++ GGJ2020_Unity/GGJ2020_Kaya/Assets/Scripts/WaveControllerScript.cs
@@ -52,7 +52,6 @@ public class WaveControllerScript : MonoBehaviour
 =======
         if (closeness > winMargin)
         {
-            Debug.Log(closeness + " " + winMargin);
             GameStateScript.State = Game.matched;
             
         }
