2bd20aeb2ac5e0d1d02f2381c6cccc9fca639ac1
Olivia <OliviaVespera@gmail.com>
2020-02-01 22:31:28 +0800

    SinglePlayerGameScene Changes

---
+++ GGJ2020_Unity/GGJ2020_Kaya/Assets/WaveControllerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveControllerScript : MonoBehaviour
+{
+    public float closeness;
+    private SineWaveScript sineWave;
+    private SquareWaveScript squareWave;
+    private TriangeWaveScript triangleWave;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sineWave = GetComponent<SineWaveScript>();
+        squareWave = GetComponent<TquareWaveScript>();
+        triangleWave = GetComponent<TriangleWaveScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
