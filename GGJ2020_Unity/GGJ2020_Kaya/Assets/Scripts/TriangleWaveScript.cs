c4fe276d4089643cd6caf874acee6864fc3e189a
Olivia <OliviaVespera@gmail.com>
2020-02-01 23:30:47 +0800

    Exposed value needed by gameaudio
    Exposed a value required by game audio for interaction sound.

--- GGJ2020_Unity/GGJ2020_Kaya/Assets/Scripts/TriangleWaveScript.cs
+++ GGJ2020_Unity/GGJ2020_Kaya/Assets/Scripts/TriangleWaveScript.cs
@@ -16,7 +16,7 @@ public class TriangleWaveScript : MonoBehaviour
     public float frequency = 1;
     public float offset = 0;
     public float direction = 1;
-
+    public bool isOn = false;
     private LineRenderer lineRenderer;
     private Vector3 pos;
     private List<Vector3> points = new List<Vector3>(); // Generated points before Simplify is used.
@@ -33,20 +33,23 @@ public class TriangleWaveScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        numberOfPoints = (int)(length/(frequency)+1);
-        lineRenderer.positionCount = numberOfPoints;
-        //creates the triangle wave from the amplitude, frequency and offset
-        var points = new Vector3[(int)(length/(frequency)+1)];
-        float step = frequency;
-        var t = Time.time;
-        for (int i = 0; i < numberOfPoints; i++)
+        if (isOn)
         {
-            Debug.Log(t%2);
-            points[i] = new Vector3(
-                pos.x + i * step+(t*direction)%frequency*2+offset,
-                pos.y + Mathf.PingPong(i*-amplitude, amplitude)*2-amplitude,
-                0.0f);
+            numberOfPoints = (int)(length/(frequency)+1);
+            lineRenderer.positionCount = numberOfPoints;
+            //creates the triangle wave from the amplitude, frequency and offset
+            var points = new Vector3[(int)(length/(frequency)+1)];
+            float step = frequency;
+            var t = Time.time;
+            for (int i = 0; i < numberOfPoints; i++)
+            {
+                Debug.Log(t%2);
+                points[i] = new Vector3(
+                    pos.x + i * step+(t*direction)%frequency*2+offset,
+                    pos.y + Mathf.PingPong(i*-amplitude, amplitude)*2-amplitude,
+                    0.0f);
+            }
+            lineRenderer.SetPositions(points);
         }
-        lineRenderer.SetPositions(points);
     }
 }
--- GGJ2020_Unity/GGJ2020_Kaya/Assets/WaveControllerScript.cs
+++ GGJ2020_Unity/GGJ2020_Kaya/Assets/WaveControllerScript.cs
@@ -5,20 +5,47 @@ using UnityEngine;
 public class WaveControllerScript : MonoBehaviour
 {
     public float closeness;
-    private SineWaveScript sineWave;
-    private SquareWaveScript squareWave;
-    private TriangleWaveScript triangleWave;
+    private SineWaveScript speakingSine;
+    private SquareWaveScript speakingSquare;
+    private TriangleWaveScript speakingTriangle;
+
+    private SineWaveScript listeningSine;
+    private SquareWaveScript listeningSquare;
+    private TriangleWaveScript listeningTriangle;
+    public GameObject speaking;
+    public GameObject listening;
+    public float amplitudeDifference;
+    public float frequencyDifference;
     // Start is called before the first frame update
     void Start()
     {
-        sineWave = GetComponent<SineWaveScript>();
-        squareWave = GetComponent<SquareWaveScript>();
-        triangleWave = GetComponent<TriangleWaveScript>();
+        speakingSine = speaking.GetComponent<SineWaveScript>();
+        speakingSquare = speaking.GetComponent<SquareWaveScript>();
+        speakingTriangle = speaking.GetComponent<TriangleWaveScript>();
+        listeningSine = listening.GetComponent<SineWaveScript>();
+        listeningSquare = listening.GetComponent<SquareWaveScript>();
+        listeningTriangle = listening.GetComponent<TriangleWaveScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(speakingSine.isOn)
+        {
+            amplitudeDifference = Mathf.Clamp(Mathf.Abs(listeningSine.amplitude-speakingSine.amplitude), 0, 1);
+            frequencyDifference = Mathf.Clamp(Mathf.Abs(listeningSine.frequency-speakingSine.frequency), 0, 1);
+            
+        }
+        else if (speakingTriangle.isOn)
+        {
+            amplitudeDifference = Mathf.Clamp(Mathf.Abs(listeningTriangle.amplitude-speakingTriangle.amplitude), 0, 1);
+            frequencyDifference = Mathf.Clamp(Mathf.Abs(listeningTriangle.frequency-speakingTriangle.frequency), 0, 1);
+        }
+        else
+        {
+            amplitudeDifference = Mathf.Clamp(Mathf.Abs(listeningSquare.amplitude-speakingSquare.amplitude), 0, 1);
+            frequencyDifference = Mathf.Clamp(Mathf.Abs(listeningSquare.frequency-speakingSquare.frequency), 0, 1);
+        }
+        closeness = 1-(amplitudeDifference + frequencyDifference)/2;
     }
 }
