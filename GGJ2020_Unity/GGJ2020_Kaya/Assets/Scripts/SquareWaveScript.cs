d459191f26e28e76ee440bc0095c88b256b78aac
Olivia <OliviaVespera@gmail.com>
2020-02-01 20:37:52 +0800

    Finalize Wave Scripts
    Includes Square and Triangle Waves inclusive of the SIne wave. has direction.

---
+++ GGJ2020_Unity/GGJ2020_Kaya/Assets/Scripts/SquareWaveScript.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareWaveScript : MonoBehaviour
+{
+    public int numberOfPoints = 10;
+    [Range(0,100)]
+    public float length = 30;
+    [Range(0,10)]
+    public float amplitude = 1;
+    [Range(0,10)]
+    public float frequency = 1;
+    public float offset = 0;
+    public float direction = 1;
+
+    private LineRenderer lineRenderer;
+    private Vector3 pos;
+    private List<Vector3> points = new List<Vector3>(); // Generated points before Simplify is used.
+
+    public void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.widthMultiplier = 0.2f;
+        numberOfPoints = (int)(length/(1/frequency)*2);
+        lineRenderer.positionCount = numberOfPoints;
+        pos = GetComponent<Transform>().position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        numberOfPoints = (int)(length/(1/frequency)*2);
+        lineRenderer.positionCount = numberOfPoints;
+        //creates the sine curve from the amplitude, frequency and offset
+        var points = new Vector3[(int)(length/(1/frequency)*2)];
+        float step = (length/(numberOfPoints/2));
+        var t = Time.time;
+        // points[0] = new Vector3(pos.x, pos.y -amplitude, 0.0f);
+        for (int i = 0; i < numberOfPoints; i+=2)
+        {
+            Debug.Log(t%2);
+
+            if(((i)/2)%2 == 0)
+            {
+            points[i] = new Vector3(
+                pos.x + i * step+(t*direction/2)%frequency*4,
+                pos.y +amplitude * -1,
+                0.0f);
+            points[i+1] = new Vector3(
+                pos.x + i * step+(t*direction/2)%frequency*4,
+                pos.y -amplitude * -1,
+                0.0f);
+
+            }
+            else
+            {
+                points[i] = new Vector3(
+                pos.x + i * step + offset+(t*direction/2)%frequency*4,
+                pos.y -amplitude * -1,
+                0.0f);
+                points[i+1] = new Vector3(
+                    pos.x + i * step + offset+(t*direction/2)%frequency*4,
+                    pos.y +amplitude * -1,
+                    0.0f);
+            }
+
+            
+        }
+        lineRenderer.SetPositions(points);
+    }
+}
--- GGJ2020_Unity/GGJ2020_Kaya/Assets/Scripts/TriangleWaveScript.cs
+++ GGJ2020_Unity/GGJ2020_Kaya/Assets/Scripts/TriangleWaveScript.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class TriangleWaveScript : MonoBehaviour
 {
     public int numberOfPoints = 10;
+    [Range(0,100)]
+
     public float length = 30;
+    [Range(0,10)]
+
     public float amplitude = 1;
+    [Range(0,10)]
+
     public float frequency = 1;
     public float offset = 0;
+    public float direction = 1;
 
     private LineRenderer lineRenderer;
     private Vector3 pos;
@@ -18,27 +25,26 @@ public class TriangleWaveScript : MonoBehaviour
     {
         lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.widthMultiplier = 0.2f;
-        lineRenderer.positionCount = (int)(numberOfPoints/(frequency)+1);
+        numberOfPoints = (int)(length/(frequency)+1);
+        lineRenderer.positionCount = numberOfPoints;
         pos = GetComponent<Transform>().position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        lineRenderer.positionCount = (int)(numberOfPoints/(frequency)+1);
-        //creates the sine curve from the amplitude, frequency and offset
-        var points = new Vector3[(int)(numberOfPoints/(frequency)+1)];
+        numberOfPoints = (int)(length/(frequency)+1);
+        lineRenderer.positionCount = numberOfPoints;
+        //creates the triangle wave from the amplitude, frequency and offset
+        var points = new Vector3[(int)(length/(frequency)+1)];
         float step = frequency;
         var t = Time.time;
         for (int i = 0; i < numberOfPoints; i++)
         {
+            Debug.Log(t%2);
             points[i] = new Vector3(
-                pos.x + i * step/2,
-                pos.y + Mathf.PingPong(i*-amplitude, amplitude),
-                0.0f);
-            points[i] = new Vector3(
-                pos.x + i * step,
-                pos.y + Mathf.PingPong(i*-amplitude, amplitude),
+                pos.x + i * step+(t*direction)%frequency*2+offset,
+                pos.y + Mathf.PingPong(i*-amplitude, amplitude)*2-amplitude,
                 0.0f);
         }
         lineRenderer.SetPositions(points);
