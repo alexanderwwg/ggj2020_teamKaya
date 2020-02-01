1276d7e462f9be449d54b0f72f837334d79f0180
Arghkeeel <32801217+Arghkeeel@users.noreply.github.com>
2020-02-01 22:50:21 +0800

    Update Head_AniScript.cs

--- GGJ2020_Unity/GGJ2020_Kaya/Assets/Scripts/Head_AniScript.cs
+++ GGJ2020_Unity/GGJ2020_Kaya/Assets/Scripts/Head_AniScript.cs
@@ -4,87 +4,106 @@ using UnityEngine;
 
 public class Head_AniScript : MonoBehaviour
 {
-    public Animator animator;
-    public bool talking;
-    public bool isLeft;
-    public float moveSpeed;
-    public float endPos;
-    public float gamePos=5.35f;
+  public Animator animator;
+  public bool talking;
+  public bool isLeft;
+  public float moveSpeed;
+  public float endPos;
+  public float gamePos = 5.35f;
 
-    public enum headState {idle, kissMove, kissFinish, beginGame };
-    public headState hS;
-    private Transform transform;
-    // Start is called before the first frame update
-    void Start()
+  public enum headState { idle, kissMove, kissFinish, beginGame, headButt };
+  public headState hS;
+  private Transform transform;
+  // Start is called before the first frame update
+  void Start()
+  {
+    if (isLeft)
     {
-        if (isLeft)
+      GetComponent<SpriteRenderer>().flipX = true;
+      gamePos *= -1;
+    }
+    SetIsTalking(talking);
+    transform = GetComponent<Transform>();
+    hS = headState.headButt;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    switch (hS)
+    {
+      case headState.idle:
         {
-            GetComponent<SpriteRenderer>().flipX = true;
-            gamePos *=-1;
+          break;
         }
-        SetIsTalking(talking);
-        transform = GetComponent<Transform>();
-        hS = headState.beginGame;
+      case headState.kissMove:
+        {
+          MoveHead(moveSpeed);
+          if (isLeft && endPos < transform.position.x)
+          {
+            hS = headState.kissFinish;
+          }
+          if (!isLeft && endPos > transform.position.x)
+          {
+            hS = headState.kissFinish;
+          }
+          break;
+        }
+      case headState.headButt:
+        {
+          
+          if (isLeft && endPos < transform.position.x +1)
+          {
+            transform.rotation = Quaternion.Euler(0, 0, -30);
+
+            hS = headState.kissFinish;
+          }
+          else if (!isLeft && endPos > transform.position.x -1)
+          {
+            transform.rotation = Quaternion.Euler(0, 0, 30);
+
+            hS = headState.kissFinish;
+          }
+          else
+           MoveHead(moveSpeed);
+          break;
+        }
+      case headState.beginGame:
+        {
+          MoveHead(moveSpeed);
+          if (isLeft && gamePos < transform.position.x)
+          {
+            hS = headState.idle;
+          }
+          if (!isLeft && gamePos > transform.position.x)
+          {
+            hS = headState.idle;
+          }
+          break;
+        }
+      default:
+        {
+          break;
+        }
+    }
+  }
+
+  public void SetIsTalking(bool talk)
+  {
+    animator.SetBool("IsTalking", talk);
+  }
+
+  public void MoveHead(float speed)
+  {
+    if (isLeft)
+    {
+      transform.Translate((Vector3.right * Time.deltaTime) * moveSpeed);
+    }
+    else
+    {
+      transform.Translate((Vector3.left * Time.deltaTime) * moveSpeed);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        switch (hS)
-        {
-            case headState.idle:
-                {
-                    break;
-                }
-            case headState.kissMove:
-                {
-                    MoveHead(moveSpeed);
-                    if(isLeft && endPos < transform.position.x)
-                    {
-                        hS = headState.kissFinish;
-                    }
-                    if(!isLeft && endPos > transform.position.x)
-                    {
-                        hS = headState.kissFinish;
-                    }
-                    break;
-                }
-            case headState.beginGame:
-            {
-                MoveHead(moveSpeed);
-                if(isLeft && gamePos < transform.position.x)
-                    {
-                        hS = headState.idle;
-                    }
-                    if(!isLeft && gamePos > transform.position.x)
-                    {
-                        hS = headState.idle;
-                    }
-                    break;
-            }
-            default:
-                {
-                    break;
-                }
-        }
-    }
-
-    public void SetIsTalking(bool talk)
-    {
-        animator.SetBool("IsTalking",talk);
-    }
-
-    public void MoveHead(float speed)
-    {
-        if (isLeft)
-        {
-            transform.Translate((Vector3.right * Time.deltaTime) * moveSpeed);
-        }
-        else
-        {
-            transform.Translate((Vector3.left * Time.deltaTime) * moveSpeed);
-        }
-        
-    }
+  }
 
 }
