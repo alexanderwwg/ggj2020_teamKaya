503a1199da1f649808f7b5ebd89159e3dc08d471
Arghkeeel <32801217+Arghkeeel@users.noreply.github.com>
2020-02-01 19:35:09 +0800

    Added progress bar prefabs
    Everything needed for the progress bar is in the ProgressBarStuff folder. Incuded is an example scene.

---
+++ GGJ2020_Unity/GGJ2020_Kaya/Assets/ProgressBarStuff/progressbar assets/ProgressBar.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+  [Tooltip("Controls the length of the progress bar")]
+  [Range(0, 1)]
+  public float progress = 0.5f;
+
+  [Header("Milestones")]
+  [Range(0, 1)]
+  public float checkPoint1 = 0;
+  [Range(0, 1)]
+  public float checkPoint2 = 0;
+  [Range(0, 1)]
+  public float checkPoint3 = 0;
+
+
+  // The image this script is attached to
+  private Renderer rend;
+
+  [Header("Component References")]
+  public Transform[] checkpoints;
+
+  private int starAmt = 0;
+
+
+  public List<SpriteRenderer> starSpriteRends;
+
+  public List<ParticleSystem> particleSystems;
+
+  private void OnValidate()
+  {
+
+    if (checkpoints == null || checkpoints[0] == null) return;
+
+    Vector3 pos1 = checkpoints[0].localPosition;
+    pos1.x = checkPoint1 - 0.5f;
+    checkpoints[0].localPosition = pos1;
+
+    Vector3 pos2 = checkpoints[1].localPosition;
+    pos2.x = checkPoint2 - 0.5f;
+    checkpoints[1].localPosition = pos2;
+
+    Vector3 pos3 = checkpoints[2].localPosition;
+    pos3.x = checkPoint3 - 0.5f;
+    checkpoints[2].localPosition = pos3;
+  }
+
+  //private void OnValidate()
+  //{
+  //  if (rend == null) rend = GetComponent<Renderer>();
+
+  //rend.material.SetFloat("_Progress", progress);
+  //}
+
+  // Start is called before the first frame update
+  void Awake()
+  {
+    rend = GetComponent<Renderer>();
+  }
+
+  private void Update()
+  {
+    if(rend.material.GetFloat("_Progress") != progress)
+    {
+      rend.material.SetFloat("_Progress", progress);
+      CheckMilestone();
+    }
+  }
+
+  private void CheckMilestone()
+  {
+    switch(starAmt)
+    {
+      case 0:
+        if (progress >= checkPoint1)
+        {
+          starAmt++;
+          starSpriteRends[0].color = Color.white;
+          particleSystems[0].Play();
+        }
+        break;
+      case 1:
+        if (progress >= checkPoint2)
+        {
+          starAmt++;
+          starSpriteRends[1].color = Color.white;
+          starSpriteRends[2].color = Color.white;
+          particleSystems[1].Play();
+        }
+        break;
+      case 2:
+        if (progress >= checkPoint3)
+        {
+          starAmt++;
+          starSpriteRends[3].color = Color.white;
+          starSpriteRends[4].color = Color.white;
+          starSpriteRends[5].color = Color.white;
+          particleSystems[2].Play();
+        }
+        break;
+    }
+  }
+
+}
---
+++ GGJ2020_Unity/GGJ2020_Kaya/Assets/ProgressBarStuff/progressbar assets/scrolling shader bar.shader
@@ -0,0 +1,78 @@
+﻿Shader "Diffuse Scroll Transparent Mask"
+{
+
+	Properties
+	{
+		_MainTex("Base (RGB)", 2D) = "white" {}
+		_AlphaTex("Mask (RGB)", 2D) = "white" {}
+		_ScrollU("Scroll Speed U", float) = 1
+		_ScrollV("Scroll Speed V", float) = 1
+		_Progress("Progress", float) = 0.5
+	}
+
+		CGINCLUDE
+
+			struct v2f_full
+		{
+			float4 pos : SV_POSITION;
+			half4 uv : TEXCOORD0;
+		};
+
+#include "UnityCG.cginc"        
+
+		sampler2D _MainTex;
+		sampler2D _AlphaTex;
+		float _ScrollU;
+		float _ScrollV;
+		float _Progress;
+
+		ENDCG
+
+			SubShader
+		{
+			Tags { "RenderType" = "Transparent" }
+			LOD 200
+			ZWrite Off
+			Blend SrcAlpha OneMinusSrcAlpha
+
+			Pass
+			{
+				Tags { "LightMode" = "Vertex" }
+
+				CGPROGRAM
+
+				half4 _MainTex_ST;
+				half4 _AlphaTex_ST;
+
+				v2f_full vert(appdata_full v)
+				{
+					v2f_full o;
+					o.pos = UnityObjectToClipPos(v.vertex);
+					o.uv.xy = TRANSFORM_TEX(v.texcoord, _MainTex);
+					o.uv.zw = TRANSFORM_TEX(v.texcoord, _AlphaTex);
+
+					o.uv.x += frac(_Time * _ScrollU);
+					o.uv.y += frac(_Time * _ScrollV);
+
+					return o;
+				}
+
+				fixed4 frag(v2f_full i) : COLOR0
+				{
+					fixed4 tex = tex2D(_MainTex, i.uv.xy);
+					fixed3 texAlpha = tex2D(_AlphaTex, i.uv.zw).rgb;
+					tex.a *= texAlpha.r < _Progress;
+
+					return tex;
+				}
+
+				#pragma vertex vert
+				#pragma fragment frag
+				#pragma fragmentoption ARB_precision_hint_fastest 
+
+				ENDCG
+			}
+		}
+
+			Fallback "Mobile/Texture"
+}
