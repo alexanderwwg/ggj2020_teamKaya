56cd0425d958aa452d3ecb3a87acc5319a1c6bad
MT-Nichols <33322116+MT-Nichols@users.noreply.github.com>
2020-02-01 18:31:06 +0800

    Updated Talking Head Prefabs
    Added functionality to the HeadAni_Script that moves each head towards each other.

--- GGJ2020_Unity/GGJ2020_Kaya/Assets/Scripts/Head_AniScript.cs
+++ GGJ2020_Unity/GGJ2020_Kaya/Assets/Scripts/Head_AniScript.cs
@@ -6,20 +6,66 @@ public class Head_AniScript : MonoBehaviour
 {
     public Animator animator;
     public bool talking;
+    public bool isLeft;
+    public float moveSpeed;
+    public float endPos;
+
+    enum headState {idle, kissMove, kissFinish };
+    private headState hS;
+    private Transform transform;
     // Start is called before the first frame update
     void Start()
     {
         SetIsTalking(talking);
+        transform = GetComponent<Transform>();
+        hS = headState.idle;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (hS)
+        {
+            case headState.idle:
+                {
+                    break;
+                }
+            case headState.kissMove:
+                {
+                    MoveHead(moveSpeed);
+                    if(isLeft && endPos < transform.position.x)
+                    {
+                        hS = headState.kissFinish;
+                    }
+                    if(!isLeft && endPos > transform.position.x)
+                    {
+                        hS = headState.kissFinish;
+                    }
+                    break;
+                }
+            default:
+                {
+                    break;
+                }
+        }
     }
 
     public void SetIsTalking(bool talk)
     {
         animator.SetBool("IsTalking",talk);
     }
+
+    public void MoveHead(float speed)
+    {
+        if (isLeft)
+        {
+            transform.Translate((Vector3.right * Time.deltaTime) * moveSpeed);
+        }
+        else
+        {
+            transform.Translate((Vector3.left * Time.deltaTime) * moveSpeed);
+        }
+        
+    }
+
 }
