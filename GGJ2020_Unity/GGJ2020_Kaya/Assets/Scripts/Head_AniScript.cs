f368fe017bad2d9a8b9112b44bef55e45d47f0ea
kurokupo <alexandthemachine@gmail.com>
2020-02-01 16:13:47 +0800

    Revert "Revert "Revert "Revert "Merge branch 'master' of https://github.com/kurokupo/ggj2020_teamKaya""""
    This reverts commit a63527e3bea432da7f70fdb3ec83d4281a1082b8.

---
+++ GGJ2020_Unity/GGJ2020_Kaya/Assets/Scripts/Head_AniScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Head_AniScript : MonoBehaviour
+{
+    public Animator animator;
+    public bool talking;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetIsTalking(talking);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetIsTalking(bool talk)
+    {
+        animator.SetBool("IsTalking",talk);
+    }
+}
