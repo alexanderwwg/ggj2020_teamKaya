3a5c3cb4eaad1fe902a1dd52d03f4dd4530dd068
MT-Nichols <33322116+MT-Nichols@users.noreply.github.com>
2020-02-01 21:37:01 +0800

    Updated Menu Features
    Title comes in from the top and buttons, start invisible but appear after title comes in. Credits panel implemented.

--- GGJ2020_Unity/GGJ2020_Kaya/Assets/Scripts/MenuButton_Script.cs
+++ GGJ2020_Unity/GGJ2020_Kaya/Assets/Scripts/MenuButton_Script.cs
@@ -6,16 +6,40 @@ using UnityEngine.SceneManagement;
 
 public class MenuButton_Script : MonoBehaviour
 {
+    public GameObject button1;
+    public GameObject button2;
+    public GameObject button3;
+    public GameObject credits;
     // Start is called before the first frame update
     void Start()
     {
-        
+        button1.SetActive(false);
+        button2.SetActive(false);
+        button3.SetActive(false);
+        credits.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ShowCredits()
+    {
+        credits.SetActive(true);
+    }
+
+    public void HideCredits()
+    {
+        credits.SetActive(false);
+    }
+
+    public void ShowButtons()
+    {
+        button1.SetActive(true);
+        button2.SetActive(true);
+        button3.SetActive(true);
     }
 
     public void LoadScene(string scene)
