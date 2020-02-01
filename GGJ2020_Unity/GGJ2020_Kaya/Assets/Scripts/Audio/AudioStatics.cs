610d2d0da5eee6a6f211ca413ce1675b7692ec5a
kurokupo <alexandthemachine@gmail.com>
2020-02-01 15:32:44 +0800

    FMOD Event setup on test scene. Use arrow keys to control event Progress.

---
+++ GGJ2020_Unity/GGJ2020_Kaya/Assets/Scripts/Audio/AudioStatics.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+
+public static class AudioStatics
+{
+    // NOT DONE
+    public static void LoadBank()
+    {
+        //RuntimeManager.LoadBank();  //will make a list for this later.
+    }
+
+    // NOT DONE
+    public static void Pause()
+    {
+        // run a override snapshot or set bus values
+    }
+
+    public static void SetListenerPos(Transform t)
+    {
+        RuntimeManager.SetListenerLocation(t);
+    }
+
+    public static void SetParameterValue(StudioEventEmitter audioEventEmitter, string name, float value)
+    {
+        audioEventEmitter.SetParameter(name, value);
+    }
+
+    public static void StopEvent(StudioEventEmitter audioEventEmitter)
+    {
+        if (audioEventEmitter.IsPlaying())
+            audioEventEmitter.Stop();
+    }
+
+    public static void PlayEvent(StudioEventEmitter audioEventEmitter)
+    {
+        audioEventEmitter.Play();
+    }
+
+    public static void PlayOneShotAtLocation(string eventPath, Vector3 pos)
+    {
+        RuntimeManager.PlayOneShot(eventPath, pos);
+    }
+
+    public static void PlayOneShotAttached(string eventPath, GameObject target)
+    {
+        RuntimeManager.PlayOneShotAttached(eventPath, target);
+    }
+
+}
---
+++ GGJ2020_Unity/GGJ2020_Kaya/Assets/Scripts/Audio/SFXEmitterUpdate.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class SFXEmitterUpdate : MonoBehaviour
+{
+    // Per Second.
+    public float updateRate = 0.25f;
+    [Range(0, 1)]
+    float progressValue;
+
+    StudioEventEmitter sfxEmitter;
+
+
+    public void UpdateProgress(float input)
+    {
+        progressValue = Mathf.Clamp(input, 0, 1);
+        sfxEmitter.SetParameter("Progress", progressValue);
+    }
+
+    private void Start()
+    {
+        sfxEmitter = gameObject.GetComponent<StudioEventEmitter>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            UpdateParameters(true);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            UpdateParameters(false);
+        }
+        print(progressValue);
+
+
+    }
+
+    void UpdateParameters(bool increase)
+    {
+        if (increase)
+        {
+            UpdateProgress(progressValue + updateRate * Time.deltaTime);
+        }
+        else
+        {
+            UpdateProgress(progressValue - updateRate * Time.deltaTime);
+        }
+    }
+
+
+}
